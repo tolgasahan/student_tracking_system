d42c34a5683f3cb78d4c972ce25d381affb6b516
Tolga <sahantolga1021567@gmail.com>
2022-11-01 15:08:54 +0300

    commit 3

--- lib/main.dart
+++ lib/main.dart
@@ -14,26 +14,27 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   String banner = "Student Tracker System";
-
+  Student selectedStudent = Student.withID(0,"", "", "", 0);
   List<Student> students = [
-    Student(
+    Student.withID(
+        1,
         "https://www.arkitera.com/wp-content/uploads/2019/12/Orhan-Koluk%C4%B1sa_.jpg.jpeg",
         "Tolga",
         "Şahan",
         25),
-    Student(
+    Student.withID(
+        2,
         "https://cdnuploads.aa.com.tr/uploads/Contents/2019/07/01/thumbs_b_c_91832c8bbc58b90b6d1ef3925cb1c328.jpg",
         "Mehmet",
         "Karaduman",
         45),
-    Student(
+    Student.withID(
+        3,
         "http://www.muratcangumus.com/wp-content/uploads/2017/04/insan-resimleri-zanzibar-beach-boy-Muratcan-Gumus-www.muratcangumus.com_.jpg",
         "Cengiz",
         "Topaloğlu",
         65)
   ];
-
-  String name = "";
 
   @override
   Widget build(BuildContext context) {
@@ -43,7 +44,13 @@ class _MyAppState extends State<MyApp> {
         ),
         body: buildBody(context));
   }
-
+void showMessage(BuildContext context, String title, String content){
+    var alert = AlertDialog(
+      title: Text(title),
+      content: Text(content),
+    );
+    showDialog(context: context, builder: (BuildContext context) => alert);
+}
   Widget buildBody(BuildContext context) {
     return Column(
       children: <Widget>[
@@ -66,22 +73,31 @@ class _MyAppState extends State<MyApp> {
                     trailing: buildStatusIcon(students[index].grade),
                     onTap: () {
                       setState(() {
-                        name = students[index].firstName +
-                            " " +
-                            students[index].lastName;
+                        selectedStudent = students[index];
                       });
                       ;
                     },
                   );
                 })),
-        Text("Selected Student: " + name),
+        Text("Selected Student: " + selectedStudent.firstName),
         Row(
           children: <Widget>[
             Flexible(
               fit: FlexFit.tight,
               flex: 1,
               child: ElevatedButton(
-                child: Text("New Student"),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.blueGrey, // Background color
+                ),
+                child: Row(
+                  children: [
+                    Icon(Icons.add),
+                    SizedBox(
+                      width: 8.0,
+                    ),
+                    Text("Add")
+                  ],
+                ),
                 onPressed: () {},
               ),
             ),
@@ -89,7 +105,18 @@ class _MyAppState extends State<MyApp> {
               fit: FlexFit.tight,
               flex: 1,
               child: ElevatedButton(
-                child: Text("Update"),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.greenAccent, // Background color
+                ),
+                child: Row(
+                  children: [
+                    Icon(Icons.update),
+                    SizedBox(
+                      width: 8.0,
+                    ),
+                    Text("Update")
+                  ],
+                ),
                 onPressed: () {},
               ),
             ),
@@ -97,8 +124,24 @@ class _MyAppState extends State<MyApp> {
               fit: FlexFit.tight,
               flex: 1,
               child: ElevatedButton(
-                child: Text("Delete"),
-                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.grey, // Background color
+                ),
+                child: Row(
+                  children: [
+                    Icon(Icons.delete),
+                    SizedBox(
+                      width: 8.0,
+                    ),
+                    Text("Delete")
+                  ],
+                ),
+                onPressed: () {
+                  setState(() {
+                    students.remove(selectedStudent);
+                  });
+                  showMessage(context, "Delete", selectedStudent.firstName+" successful deleted");
+                },
               ),
             ),
           ],
--- lib/models/student.dart
+++ lib/models/student.dart
@@ -1,26 +1,32 @@
-class Student{
+class Student {
+  int id = 0;
   String photo = "";
   String firstName = "";
   String lastName = "";
   int grade = 0;
   String _status = "";
 
-  Student(String photo, String firstName, String lastName ,int grade){
+  Student(String photo, String firstName, String lastName, int grade) {
+    this.photo = photo;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.grade = grade;
+  }
+  Student.withID(int id, String photo, String firstName, String lastName, int grade) {
+    this.id = id;
     this.photo = photo;
     this.firstName = firstName;
     this.lastName = lastName;
     this.grade = grade;
   }
 
-  String get getStatus{
-    if(this.grade >= 50){
+  String get getStatus {
+    if (this.grade >= 50) {
       return "Successful";
-    }else if(this.grade >= 40){
+    } else if (this.grade >= 40) {
       return "Make-up Exam";
-    }else{
+    } else {
       return "Fail";
     }
-
   }
-
 }
